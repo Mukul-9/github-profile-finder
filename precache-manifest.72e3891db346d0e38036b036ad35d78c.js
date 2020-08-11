11360eb2684f21517558f20f3c3bc1e8a8e44e86
Mukul Choudhury <mukulc998@gmail.com>
2020-08-11 10:40:16 +0530

    Updates

---
+++ precache-manifest.72e3891db346d0e38036b036ad35d78c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21be5b630d2d46b6faa308b0270f5349",
+    "url": "/github-profile-finder/index.html"
+  },
+  {
+    "revision": "88372ae7fae18718efbf",
+    "url": "/github-profile-finder/static/css/main.cec29002.chunk.css"
+  },
+  {
+    "revision": "d7f441ba63faabd7fe78",
+    "url": "/github-profile-finder/static/js/2.49281ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profile-finder/static/js/2.49281ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88372ae7fae18718efbf",
+    "url": "/github-profile-finder/static/js/main.f7c668bd.chunk.js"
+  },
+  {
+    "revision": "6377305564d7756b6639",
+    "url": "/github-profile-finder/static/js/runtime-main.20269aa4.js"
+  },
+  {
+    "revision": "7e80f5c029a503fff2cd20e5e837a086",
+    "url": "/github-profile-finder/static/media/gitlogo.7e80f5c0.png"
+  }
+]);
